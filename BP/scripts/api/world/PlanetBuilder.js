557cc1de8664b3dad7d9850d58d6e786626ec411
Захаричек <174832353+Zaharichek@users.noreply.github.com>
2025-07-31 17:23:56 +0300

    moon dungeon generation

--- BP/scripts/api/world/PlanetBuilder.js
+++ BP/scripts/api/world/PlanetBuilder.js
@@ -1,4 +1,5 @@
 import { world, BlockVolume, system } from "@minecraft/server"
+import { Planet } from "../../planets/dimension/GalacticraftPlanets"
 
 function chunk_corner({x, z}) {
     return {
@@ -12,19 +13,29 @@ system.beforeEvents.startup.subscribe(({blockComponentRegistry}) => {
     blockComponentRegistry.registerCustomComponent('cosmos:end_cleaner', {
         onTick({block}) {
             const the_end = world.getDimension('the_end');
-            const {x, z} = chunk_corner(block.location)
+            let location = block.location;
+            const {x, z} = chunk_corner(location)
             const first_corner = {x, y: 10, z}
             const other_corner = {x: x + 15, y: 120, z: z + 15} 
             the_end.fillBlocks(new BlockVolume(first_corner, other_corner), 'air', {
                 blockFilter: {includeTypes: ['end_stone', 'bedrock', 'end_gateway', 'chorus_plant', 'chorus_flower']}
             })
-            the_end.setBlockType(block.location, block.below().type)
+            the_end.setBlockType(location, block.below().type)
             const shulkers = the_end.getEntities({
                 location: first_corner,
                 volume: {x: 16, y: 210, z: 16},
                 type: "minecraft:shulker",
             })
             shulkers.forEach(shulker=> shulker.remove())
+            
+            let planet = Planet.getAll().find(pl => pl.isOnPlanet(location));
+            let offset = planet.offset(location)
+            offset.x = offset.x - 8;
+            offset.z = offset.z - 8;
+            if(planet.type == "moon" && (offset.x != 8 || offset.z != 8) && ((offset.x % 1024 === 0 || !offset.x) && (offset.z % 1024 === 0 || !offset.z))){
+                let top_block = the_end.getTopmostBlock({x: location.x, z: location.z}).location.y;
+                world.structureManager.placeJigsawStructure("cosmos:moon_dungeon", the_end, {x: location.x, y: top_block - 20, z: location.z}, {ignoreStartHeight: true});
+            }
         }
     })
 })
