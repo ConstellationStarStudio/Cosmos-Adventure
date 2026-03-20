1e8358f205af2919179f50965bbbc34759325751
Захаричек <174832353+Zaharichek@users.noreply.github.com>
2026-03-20 16:08:32 +0300

    intital commit

--- BP/scripts/core/vehicles/AllVehicles.js
+++ BP/scripts/core/vehicles/AllVehicles.js
@@ -1,6 +1,7 @@
 import Rocket from "./rockets/Rocket";
 import MoonLander from "./landers/MoonLander";
 import LandingBalloons from "./landers/LandingBalloons";
+import Buggy from "./Buggy";
 
 export default  {
     "cosmos:rocket_tier_1": {
@@ -9,7 +10,8 @@ export default  {
 		tier: 1,
 		inventory_index: 2,
 		speed: 1,
-		camera: "minecraft:follow_orbit"
+		camera: "minecraft:follow_orbit",
+		drops_item: true
 
 	},
 	"cosmos:rocket_tier_2": {
@@ -18,16 +20,25 @@ export default  {
 		tier: 2,
 		inventory_index: 2,
 		speed: 2,
-		camera: "cosmos:rocket_camera"
+		camera: "cosmos:rocket_camera",
+		drops_item: true
 	},
 	"cosmos:lander": {
 		ui: '§f§u§e§l§_§c§h§e§s§t§',
 		class: MoonLander,
-		inventory_index: 4
+		inventory_index: 4,
+	    drops_item: false
 	},
 	"cosmos:landing_balloons": {
 		ui: '§f§u§e§l§_§c§h§e§s§t§',
 		class: LandingBalloons,
-		inventory_index: 4
+		inventory_index: 4,
+		drops_item: false
 	}, 
+	"cosmos:moon_buggy": {
+		ui: '§f§u§e§l§',
+		class: Buggy,
+		inventory_index: 2,
+		drops_item: true
+	}
 }
--- BP/scripts/core/vehicles/Vehicle.js
+++ BP/scripts/core/vehicles/Vehicle.js
@@ -1,7 +1,12 @@
 import {system, world, ItemStack} from "@minecraft/server";
 import AllVehicles from "./AllVehicles";
 import { rocket_nametags } from "../../api/player/liftoff";
+
 export let vehicles = new Map();
+export const pads = {
+	"cosmos:buggy_fueling_pad": "buggy",
+	"cosmos:rocket_launch_pad": "rocket",
+}
 
 function reload_vehicle(entity){
     if (!Object.keys(AllVehicles).includes(entity.typeId) || vehicles.has(entity.id)) return;
@@ -21,7 +26,7 @@ world.afterEvents.worldLoad.subscribe(() => {
             if(!vehicle?.isValid) return;
             const data = AllVehicles[vehicle.typeId]
             // tick the vehicle
-            new data.class(vehicle)
+            data.class(vehicle)
         });
     });
 });
@@ -29,10 +34,24 @@ world.afterEvents.worldLoad.subscribe(() => {
 world.afterEvents.entitySpawn.subscribe(({entity}) => {
     if(AllVehicles[entity.typeId]){
         reload_vehicle(entity)
+        let inventory = entity.getComponent("minecraft:inventory");
+        if(!inventory) return;
         let data = get_vehicle_data(entity)
-        let inventory_size = entity.getComponent("minecraft:inventory").inventorySize - data.inventory_index;
+        const inventory_size = inventory.inventorySize - data.inventory_index;
         entity.nameTag = data.ui + rocket_nametags[inventory_size];
     }
+});
+//removes entity from cycle and drops it's item
+world.afterEvents.entityDie.subscribe(({deadEntity: entity, damageSource}) => {
+    if(AllVehicles[entity.typeId]){
+        vehicles.delete(entity.id);
+        if(!AllVehicles[entity.typeId].drops_item || (damageSource?.damagingEntity.typeId == "minecraft:player" && damageSource?.damagingEntity.getGameMode() == "Creative")) return;
+        const item = get_vehicle_item(entity, entity.getComponent("minecraft:inventory"));
+        entity.dimension.spawnItem(item, entity.location);
+    }
+});
+world.afterEvents.entityRemove.subscribe(({typeId, removedEntityId}) => {
+    if(AllVehicles[typeId]) vehicles.delete(removedEntityId)
 });
 
 export function get_vehicle_data(vehicle){
@@ -53,4 +72,11 @@ export function set_items_to_vehicle(vehicle, size, items_to_set, typeId){
     rocket_item.setLore([`§r§7Storage Space: ${size}`])
     rocket_item.setDynamicProperty("inventory_size", size)
     container.setItem(inventorySize - 1, rocket_item)
+}
+export function get_vehicle_item(vehicle, inventory){
+    const inventory_size = inventory ? inventory.inventorySize - get_vehicle_data(vehicle).inventory_index: 0;
+    let item = new ItemStack(vehicle.typeId + "_item");
+    item.setDynamicProperty("inventory_size", inventory_size);
+    item.setLore([`§r§7Storage Space: ${inventory_size}`]);
+    return item;
 }
