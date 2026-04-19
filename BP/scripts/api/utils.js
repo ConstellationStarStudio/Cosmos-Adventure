e1f972761e37674a7ed96ef2fe91ec00c83a1b60
Захаричек <174832353+Zaharichek@users.noreply.github.com>
2026-04-20 00:31:14 +0300

    Update utils.js

--- BP/scripts/api/utils.js
+++ BP/scripts/api/utils.js
@@ -8,9 +8,8 @@ const data_maps = {
 	"vehicle_data": vehicles
 }
 export function load_dynamic_object(storage, type, name = 'variables'){
-	let data = data_maps[type].get(storage.id)?.entity_data;
-	data = data ? data[name] : {};
-	return data;
+	const data = data_maps[type].get(storage.id)?.entity_data[name];
+	return data ?? {};
 }
 
 export function save_dynamic_object(storage, value, type, name = 'variables'){
